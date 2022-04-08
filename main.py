b6c5bd2311cdde9e3d37087b03982ecea07261a2
Nikunj Patel <101759820+n11kunj@users.noreply.github.com>
2022-04-08 00:32:28 +0000

    dictionary trial 1

--- main.py
+++ main.py
@@ -8,14 +8,9 @@ print("welcome to my quiz!", name)
 print("\n")
 print("About the quiz: My quiz is abut videogames tip dont type out the option instead type out the letter in lowercase enjoy!")
 print("\n")
-answer = input("What is the best selling videogame of all time? \na: Minecraft\nb: Call Of Duty Black Ops\nc: Grand Theft Auto 5\n")
-score=0
-if answer == 'a':
-  score+=1
-  print("correct")
-else:
-  print("incorrect, the correct answer was a")
-  print("score: ", score)
+
+
+n_questions ={''}
    
 
 
